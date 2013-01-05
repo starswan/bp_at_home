f23b9d054dfd5806754321c9dd73333a68323c87
Edward Wallitt <edwallitt@me.com>
2013-01-05 15:50:17 +0000

    setup generators in application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module BpAtHome
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    # Custom the generators
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.test_framework :rspec, fixture: true
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
