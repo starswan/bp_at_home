3956888906ce4c08f5746c8448a15ff52c58859e
Edward Wallitt <edwallitt@me.com>
2013-03-01 02:27:57 +0000

    fixed bug on doctors screen and added copy to homepage

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,18 +2,17 @@ class PatientsController < ApplicationController
   before_filter :authorize_doctor!
 
   def index
-   @doctor = Doctor.find(params[:doctor_id]) 
+   @doctor = Doctor.find(params[:doctor_id])
    @patients = @doctor.patients
   end
 
   def show
-    @patient = Patient.find(params[:id]) 
+    @patient = Patient.find(params[:id])
     @readings = @patient.readings.order(:created_at)
     @sys_average = @patient.sys_average
     @dys_average = @patient.dys_average
     @pulse_average = @patient.pulse_average
-    @chart = @patient.readings_chart
-    
+
     @reading = @patient.readings.new
     render 'readings/index'
   end
@@ -23,13 +22,13 @@ class PatientsController < ApplicationController
     identifier = generate_random_identifier
     patient = @doctor.patients.create(identifier: identifier)
     if patient.save
-      redirect_to doctor_patients_path(@doctor), notice: "Patient successfully added. You can now give them their unique identifier. Be sure to record it in the notes as well."
+      redirect_to doctor_patients_path(@doctor), notice: "Patient successfully added. You can now give them their unique identifier. Be sure to record it in the patient record as well."
     else
       flash.now.alert = "Something went wrong. Please try again"
       render :index
     end
   end
-  
+
   private
 
     def generate_random_identifier
