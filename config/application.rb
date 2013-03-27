e27fe63d1e71ba9e4e907270b10675364f5ebb56
Edward Wallitt <edwallitt@me.com>
2013-03-27 00:58:19 +0000

    add activeadmin css and js to precompile list

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,8 @@ module BpAtHome
 
     # For heroku
     config.assets.initialize_on_precompile = false
-
+    config.assets.precompile += ['active_admin.css', 'active_admin/print.css', 'active_admin.js']
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
